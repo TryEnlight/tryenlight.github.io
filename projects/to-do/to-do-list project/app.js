9a2ec79f80c185d2ac2e2b4b32aa26bd750c3d27
Samay Shamdasani <techboss302@gmail.com>
2016-11-14 22:32:18 -0500

    to-do project done mostly

--- projects/to-do/to-do-list project/app.js
+++ projects/to-do/to-do-list project/app.js
@@ -2,10 +2,15 @@ function newItem() {
 	var item = document.getElementById('input').value;
 	var ul = document.getElementById("list");
 	var li = document.createElement('li');
-  	li.appendChild(document.createTextNode(item));
+  	li.appendChild(document.createTextNode("- "+item));
   	ul.appendChild(li);
+  	li.onclick = removeItem;
 }
 
+
+function removeItem(e) {
+    e.target.parentElement.removeChild(e.target);
+}
 
 
 document.body.onkeyup = function(e){
