7e7671505ee136f6c94315d947d5f62a5a6af721
Samay Shamdasani <techboss302@gmail.com>
2016-11-15 22:56:12 -0500

    new project + updates! 🚀

--- projects/to-do/to-do-project/app.js
+++ projects/to-do/to-do-project/app.js
@@ -2,19 +2,23 @@ function newItem() {
 	var item = document.getElementById('input').value;
 	var ul = document.getElementById("list");
 	var li = document.createElement('li');
-  	li.appendChild(document.createTextNode("- "+item));
-  	ul.appendChild(li);
-  	li.onclick = removeItem;
+  li.appendChild(document.createTextNode("- "+item));
+  ul.appendChild(li);
+  document.getElementById('input').value="";
+  li.onclick = removeItem;
 }
-
-
-function removeItem(e) {
-    e.target.parentElement.removeChild(e.target);
-}
-
 
 document.body.onkeyup = function(e){
       if(e.keyCode == 13){
         newItem();
       }
   }
+
+function removeItem(e) {
+  e.target.parentElement.removeChild(e.target);
+}
+
+
+
+
+
