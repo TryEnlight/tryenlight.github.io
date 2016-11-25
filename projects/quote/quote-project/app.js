1742292a923b006643740188c50a6b5839ec7e48
Samay Shamdasani <techboss302@gmail.com>
2016-11-25 09:44:09 -0500

    fixing quote project

--- projects/quote/quote-project/app.js
+++ projects/quote/quote-project/app.js
@@ -1,22 +1,22 @@
 $(window).keypress(function(e) {
     if (e.which === 32) {
-       $("#quote").addClass("reset");
-       $("#quote").removeClass("executed");
-       $("#writer").toggleClass("fade");
-       setTimeout(function(){ 
-           $.ajax({
-               crossOrigin: true,
-               url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
-               dataType:"jsonp"
-           });
-       }, 1000); 
+      $("#quote").addClass("reset");
+      $("#quote").removeClass("executed");
+      $("#writer").toggleClass("fade");
+      setTimeout(function(){ 
+        $.ajax({
+          crossOrigin: true,
+          url: "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback",
+          dataType:"jsonp"
+      });
+      }, 1000); 
     }
 });
 function mycallback(json){
-   var quote = json[0];
-   $("#quote").html(quote.content)
-   $("#writer").html(quote.title)
-   $("#quote").addClass("executed");
-   $("#quote").removeClass("reset");
-   $("#writer").toggleClass("fade");
+  var quote = json[0];
+  $("#quote").html(quote.content)
+  $("#writer").html(quote.title)
+  $("#quote").addClass("executed");
+    $("#quote").removeClass("reset");
+    $("#writer").toggleClass("fade");
 }
