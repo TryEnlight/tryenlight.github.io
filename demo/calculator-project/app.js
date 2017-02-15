5c8670b2ff0145538646b5b4c4ddd6d0ac3fea4f
sravan <sravan.s@flytxt.com>
2017-02-15 16:06:37 +0530

    [WIP] Calculator functionality
    * Add data attributes to identify buttons
    * Call specific functions according to buttons clicked

--- demo/calculator-project/app.js
+++ demo/calculator-project/app.js
@@ -1,2 +1,45 @@
 // https://dribbble.com/shots/3125651-Daily-UI-004-Calculator
 // trying to build this
+
+function generateNewState() {
+  var newState = {
+    numA: null,
+    numB: null,
+    operation: null
+  };
+  return newState;
+}
+
+function addNumber() {
+}
+
+function addOperand() {
+}
+
+function calculate() {
+}
+
+function calculateAction(state, newAction, newValue) {
+  switch (newAction) {
+    case 'number':
+      return addNumber(state, newValue);
+    case 'operation':
+      return addOperand(state, newValue);
+    case 'calculate':
+      return calculate(state, newValue);
+    default: // if something goes wrong, resets state
+      return generateNewState();
+  }
+}
+
+function handleClick(event) {
+  var target = event.target;
+  var action = target.getAttribute('data-action');
+  var value = target.getAttribute('data-value');
+  calculatorState = calculateAction(calculatorState, action, value);
+}
+
+var calculator = document.getElementById('calc');
+var calculatorState = generateNewState();
+
+calculator.addEventListener('click', handleClick);
