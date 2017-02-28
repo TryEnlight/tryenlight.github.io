d6bf4bd1fd9576e98d357d1190aaf69b635c33d0
Samay Shamdasani <techboss302@gmail.com>
2017-02-28 13:19:09 -0500

    deleting static

--- _site/demo/text-editor-project/app.js
+++
@@ -1,9 +0,0 @@
-
-document.getElementById('heading').innerHTML = localStorage['title'] || 'Just Write';
-document.getElementById('content').innerHTML = localStorage['text'] || 'This text is automatically saved every second :) ';
-
- setInterval(function() {
-      localStorage['title'] = document.getElementById('heading').innerHTML;
-      localStorage['text'] = document.getElementById('content').innerHTML;
- }, 1000);
-
